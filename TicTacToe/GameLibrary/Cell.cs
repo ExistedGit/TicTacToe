cf322bc803f77ad1528baaa56d42b6c006e6e197
ExistedGit <paytvemail@gmail.com>
2022-05-07 15:30:14 +0300

    + Common game library

---
+++ TicTacToe/GameLibrary/Cell.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary
+{
+    public struct Cell
+    {
+        public enum CellState
+        {
+            Empty,
+            Cross,
+            Circle
+        }
+        int X, Y;
+        CellState State;
+        public Cell(int x, int y)
+        {
+            X = x;
+            Y = y;
+            State = CellState.Empty;
+        }
+    }
+}
