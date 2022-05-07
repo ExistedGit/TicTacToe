33c32464daa54443741ba6dfdf3652d74d066e71
DedFama <clash2clans1one@gmail.com>
2022-05-07 20:12:40 +0200

    InfoStartGameMessage

---
+++ TicTacToe/GameLibrary/InfoStartGameMessage.cs
@@ -0,0 +1,18 @@
+﻿using MessageLibrary;
+
+namespace GameLibrary
+{
+    public class InfoStartGameMessage : Message
+    {
+        public string EnemyUserName { get; set; }
+        public bool IsYourTurn { get; set; }
+
+        public InfoStartGameMessage(string EnemyUserName, bool IsYourTurn)
+        {
+            this.EnemyUserName = EnemyUserName;
+            this.IsYourTurn = IsYourTurn;
+        }
+
+
+    }
+}
