aad00a73e56a7c775c7d4a4cf44985cbc31a393c
DedFama <clash2clans1one@gmail.com>
2022-05-07 20:48:12 +0200

    GameInfoMessage: update

--- TicTacToe/GameLibrary/GameInfoMessage.cs
+++ TicTacToe/GameLibrary/GameInfoMessage.cs
@@ -6,10 +6,20 @@ namespace GameLibrary
     public class GameInfoMessage : Message
     {
         public Cell UpdatedCell { get; set; }
+        public bool isGameEnded { get; set; }
+        public bool isWinner { get; set; }
+
 
         public GameInfoMessage(Cell UpdatedCell)
         {
             this.UpdatedCell = UpdatedCell;
+        }
+
+        public GameInfoMessage(Cell UpdatedCell, bool isGameEnded, bool isWinner)
+        {
+            this.UpdatedCell = UpdatedCell;
+            this.isGameEnded = isGameEnded;
+            this.isWinner = isWinner;
         }
 
     }
