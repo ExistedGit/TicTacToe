b6d3f4ae4ea8e7da1e4b88afc772e94cbb57c838
ExistedGit <paytvemail@gmail.com>
2022-05-08 19:18:27 +0300

    + Расчёт победы

--- TicTacToe/Server/GameRoom.cs
+++ TicTacToe/Server/GameRoom.cs
@@ -36,6 +36,44 @@ namespace Server
             Player1.Client.SendAsync(new StartGameMessage(Player2.UserName, Id, turn, CellState.Cross));
             Player2.Client.SendAsync(new StartGameMessage(Player1.UserName, Id, !turn, CellState.Circle));
         }
+        private bool HorizontalCheck(Cell cell)
+        {
+            for(int i =0;i < 3; i++)
+                if (Cells[cell.Y - 1, i].State != cell.State)
+                    return false;
+            
+            return true;
+        }
+        private bool VerticalCheck(Cell cell)
+        {
+            for (int i = 0; i < 3; i++)
+                if (Cells[i, cell.X - 1].State != cell.State)
+                    return false;
+            return true;
+        }
+        private bool DiagonalCheck(Cell cell)
+        {
+
+            // Находится ли клетка на диагонали
+            int x = cell.X - 1, y = cell.Y - 1;
+            if (x != 1 && y != 1)
+                return false;
+            bool counter = true;
+            // Правая диагональ 0/2 1/1 2/0
+            for (int i = 0, j = 2; i < 3; i++, j--)
+                if (Cells[j, i].State != cell.State)
+                    counter = false;
+            if(counter) return counter;
+            // Левая диагональ 2/2 1/1 0/0
+            for (int i = 2, j = 2; i > 0; i--, j--)
+                if (Cells[j, i].State != cell.State)
+                    return false;
+            return true;
+        }
+        private bool CheckWin(Cell cell)
+        {
+            return HorizontalCheck(cell) || VerticalCheck(cell) || DiagonalCheck(cell);
+        }
         public void MessageReceived(TcpClientWrap client, Message msg)
         {
             if(msg is GameInfoMessage)
@@ -43,12 +81,20 @@ namespace Server
                 GameInfoMessage gameInfo = msg as GameInfoMessage;
                 if(gameInfo.Id == Id)
                 {
-                    
                     Cell cell = gameInfo.UpdatedCell;
                     Console.WriteLine(WhoseTurn.UserName+ ": " + gameInfo.UpdatedCell.X + " " + gameInfo.UpdatedCell.Y);
-                    //Cells[cell.X - 1, cell.Y - 1] = cell;
-                    WhoseTurn = WhoseTurn == Player1 ? Player2 : Player1;
-                    WhoseTurn.Client.SendAsync(gameInfo);
+                    Cells[cell.Y - 1, cell.X - 1] = cell;
+                    if (!CheckWin(cell))
+                    {
+                        WhoseTurn = WhoseTurn == Player1 ? Player2 : Player1;
+                        WhoseTurn.Client.SendAsync(gameInfo);
+                    }
+                    else
+                    {
+                        //WhoseTurn.Client.SendAsync(new GameInfoMessage())
+                        WhoseTurn = null;
+
+                    }
                 }
             }
         }
